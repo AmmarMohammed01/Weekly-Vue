bea2ebe7229b902ce48e024e973eb7190e1def2e
Ammar Mohammed <ammar.hm321@gmail.com>
2025-03-07 23:52:15 -0600

    One week skeleton

---
+++ weekly-vue/src/components/day-box.vue
@@ -0,0 +1,53 @@
+<script setup>
+import {ref} from 'vue';
+const tasks = ref(['t1', 't2', 't3']);
+const newTask = ref("");
+
+const addTask = () => {
+  if(newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value);
+    newTask.value = '';
+  }
+}
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+}
+</script>
+
+<template>
+  <!-- <p>Day Box</p> -->
+  <div class="day-box">
+  <form @submit.prevent="addTask">
+    <p class="day-name">Day Name</p>
+    <label for="newTask">Add Task</label>
+    <input type="text" name="newTask" id="newTask" v-model="newTask">
+    <button type="submit">Submit</button>
+  </form>
+
+  <ul>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>
+        {{ task }}
+      </span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
+  </ul>
+
+  </div>
+</template>
+
+<style scoped>
+.day-box {
+  width: 300px;
+  height: 400px;
+  border-style: solid;
+  border-radius: 20px;
+  border-color: black;
+  margin: 10px;
+}
+
+.day-name {
+  font-size: 20px;
+}
+</style>
---
+++ weekly-vue/src/components/week-box.vue
@@ -0,0 +1,23 @@
+<script setup>
+import DayBox from "./day-box.vue";
+</script>
+
+<template>
+  <p>Week Box</p>
+  <div class="week-box">
+    <DayBox/>
+    <DayBox/>
+    <DayBox/>
+    <DayBox/>
+    <DayBox/>
+    <DayBox/>
+    <DayBox/>
+  </div>
+</template>
+
+<style scoped>
+.week-box {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+}
+</style>
